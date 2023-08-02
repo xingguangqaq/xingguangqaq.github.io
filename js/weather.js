37339443f7e03265ab4e15b1cd02365d35f34893
xingguangqaq <q2463286010@163.com>
2023-08-02 19:07:31 +0800

    Site updated: 2023-08-02 19:07:31

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+<div id="he-plugin-simple"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "666666",
+    "tmpSize": "16",
+    "cityColor": "666666",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "5c9d853123b54dbca66f70a209bb122a"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
